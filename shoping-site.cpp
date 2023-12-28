40616c8bba10b492061e5e2a827b812dbbaafee7
Yab <yeabalex18@gmail.com>
2023-12-28 10:50:20 -0800

    database-made

---
+++ shoping-site.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+   
+   //Database
+    string catagoryDb[] = {"Clothing", "Gaming", "Self-Care", "Electronics"};
+    string clothingMaleItemDb[5][2] = {{"Cargo Pants", "900"}, {"T-shirt", "500"}, {"Trouser", "1200"}, {"Ski mask", "400"}, {"Silk durag", "300"}};
+    string clothingFemaleItemDb[3][2] = {{"Oversized pants", "1000"}, {"Tote bag", "700"}, {"Allstar", "3000"}};
+    string gamingItemsDb[4][2] = {{"Playstation 4", "20000"},{"GTA V", "1000"},{"VR Headset", "3000"},{"VR Stick", "10000"}};
+    string selfCareItemDb[4][2] = {{"Cerave", "3000"},{"Nivea", "600"},{"Face mask", "400"},{"Vibe Shampoo", "2000"}};
+    string electronicsItemDb[4][2] = {{"Iphone XR", "21000"}, {"LG widescreen", "40000"}, {"Lenovo", "54000"}, {"Samsung s21", "60000"}};
+    //
+
+    
+}
