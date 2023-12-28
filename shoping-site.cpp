873a5c160ce2915830cf03780bff92d860d3bc9e
Yab <yeabalex18@gmail.com>
2023-12-28 12:07:39 -0800

    stable-commit

--- shoping-site.cpp
+++ shoping-site.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
+#include<cstdlib>
 using namespace std;
+
+#define COMMON_UI_STRING "Welcome to QuirkyMart\t\t\t\t(Your Balance is " << balance << " Birr)\n\n"
 
 int main(){
    
@@ -13,5 +16,50 @@ int main(){
     string electronicsItemDb[4][2] = {{"Iphone XR", "21000"}, {"LG widescreen", "40000"}, {"Lenovo", "54000"}, {"Samsung s21", "60000"}};
     //
 
-    
+    string ussdCode;
+    getline(cin, ussdCode);
+    int catagorySize = sizeof(catagoryDb)/sizeof(catagoryDb[0]);
+    string cartStoreArray[][2] = {};
+    if (ussdCode == "*910#") {
+        double balance = 0;
+        char choice;
+        while (true)
+        {
+        cout <<COMMON_UI_STRING;
+        cout << "1. Explore todays market\n";
+        cout << "2. Cart\n";
+        cout << "3. Recharge Balance\n";
+        cout << "4. Help\n";
+        cout << "X. Exit\n\n";
+        cout << "Enter your choice: ";
+        cin>>choice;
+        if(toupper(choice)=='X')break;
+        switch (tolower(choice))
+        {
+        case '1':
+              cout<<COMMON_UI_STRING;
+              cout<<"Choose category you want to buy(* to exit)\n";
+              for(int i=0; i<catagorySize; i++){
+                 cout<<i+1<<".  "<<catagoryDb[i]<<endl;
+              }
+              cout<<"Enter your choice: ";
+              cin>>choice;
+                  if(choice=='*')break;    
+        case '3':
+             double amount;
+             cout << "Enter the recharge amount: ";
+             cin >> amount;
+             balance += amount;
+             cout << "Balance recharged successfully. Your new balance is: " << balance << " Birr\n";
+             break;
+        
+        default:
+        cout<<"\nInvalid Choice! Please enter again.\n\n";
+            break;
+        }
+        }
+      
+    }
+
+    return 0;
 }
